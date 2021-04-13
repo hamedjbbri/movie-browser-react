dd68f14df0d57e0ab885ed73ca5307eb6ab37650
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-04-13 14:31:22 -0700

    Footer done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
- import './App.css';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
 import Header from './components/Header/Header'
+import SimpleBottomNavigation from './components/MainNav';
+import Movies from './components/Pages/Movies/Movies';
+import Search from './components/Pages/Search/Search';
+import Series from './components/Pages/Series/Series';
+import Trending from './components/Pages/Trending/Trending';
+
 function App() {
   return (
-    <div className="App">
-     <Header />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <div className="App">
+      <Container> 
+        <Switch>
+          <Route path='/' component={Trending} exact />
+          <Route path='/movies' component={Movies} />
+          <Route path='/series' component={Series} />
+          <Route path='/search' component={Search} />
+        </Switch>
+      </Container>
+      </div>
+     
+      <SimpleBottomNavigation />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Pages/Search/Search.js
@@ -0,0 +1,10 @@
+const Search = () => {
+    return (
+        <div>
+            <span className='pageTitle'>Search</span>
+            
+        </div>
+    )
+}
+
+export default Search
